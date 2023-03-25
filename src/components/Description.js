80dbdb7b58252e4269b3788147a8ab1c9b8f66c1
queen-x-vee <valentinabuoro4@gmail.com>
2023-03-25 09:04:32 +0100

    sixth commit

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import Quotation from "./../assets/Quotation.png"
-import Client from "./../assets/ClientPicture.png"
-import Adiva from "./../assets/ADIVA.png"
-import Afit from "./../assets/AFIT.png"
-import Carbon from "./../assets/Carbon.png"
+import Quotation from "./../assets/description/Quotation.png"
+import Client from "./../assets/description/ClientPicture.png"
+import Adiva from "./../assets/description/ADIVA.png"
+import Afit from "./../assets/description/AFIT.png"
+import Carbon from "./../assets/description/Carbon.png"
+import Resume from "./../assets/description/Resume.png"
+import Innovation from "./../assets/description/Innovation.png"
+import DollarCoin from "./../assets/description/DollarCoin.png"
+import People from "./../assets/description/People.png"
+import Lawyer from "./../assets/description/Lawyer.png"
+import UserShield from "./../assets/description/UserShield.png"
 
 const Description = () => {
   return (
     <>
       <div className="bg-black">
-        <div className="lg:mx-auto lg:px-20 lg:py-24 flex flex-row gap-x-32">
+        <div className="lg:mx-auto lg:px-20 lg:py-24 flex flex-row space-x-32">
           <div className="flex flex-col">
             
               <p className="lg:font-semibold lg:text-5xl lg:leading-10 lg:mb-6 text-white">
@@ -30,26 +36,32 @@ const Description = () => {
               </button>
             </div>
           </div>
-          <div className="flex flex-row space-x-20">
+          <div className="flex flex-row space-x-16">
             <div className="lg:font-normal lg:text-base lg:text-white lg:text-left">
-              <div className="lg:mb-16">
-                <p >Extensive & Fast Hiring of talents between 24-48 Hours</p>
+              <div className="lg:mb-16 lg:flex lg:flex-row lg:space-x-4">
+                <Image src={Resume} alt='resume' className="w-6 h-6"/>
+                <p className="">Extensive & Fast Hiring of talents between 24-48 Hours</p>
               </div>
-              <div className="lg:mb-16">
+              <div className="lg:mb-16 lg:flex lg:flex-row lg:space-x-4">
+                <Image src={Innovation} alt='innovation' className="w-6 h-6"/>
                 <p >A wide range of experienced talents available & within reach</p>
               </div>
-              <div className="lg:mb-16">
+              <div className="lg:mb-16 lg:flex lg:flex-row lg:space-x-4">
+                <Image src={DollarCoin} alt='dollar coin' className="w-6 h-6"/>
                 <p>Handle all billing-related support queries for you</p>
               </div>
             </div>
             <div className="lg:font-normal lg:text-base lg:text-white lg:text-left ">
-              <div className="lg:mb-16">
+              <div className="lg:mb-16 lg:flex lg:flex-row lg:space-x-4">
+                <Image src={People} alt='people'className="w-6 h-6" />
                 <p>We find, manage & keep training only the best talents</p>
               </div>
-              <div className="lg:mb-16">
+              <div className="lg:mb-16 lg:flex lg:flex-row lg:space-x-4">
+                <Image src={Lawyer} alt='lawyer' className="w-6 h-6"/>
                 <p>Easy access world class tech talent for Full-Time, Remote or Hybrid Roles</p>
               </div>
-              <div className="lg:mb-16">
+              <div className="lg:mb-16 lg:flex lg:flex-row lg:space-x-4 ">
+                <Image src={UserShield} alt='user shield' className="w-6 h-6"/>
                 <p>Proper vetting of numerous applications to find the finest talent for our clients</p>
               </div>
             </div>
@@ -76,6 +88,11 @@ const Description = () => {
             </div>
 
           </div>
+          <div className="lg:flex lg:flex-row space-x-16 justify-center h-16 mt-20 py-5">
+              <Image src={Adiva} alt='adiva'/>
+              <Image src={Afit} alt='afit'/>
+              <Image src={Carbon} alt='carbon'/>
+            </div>
         </div>
       </div>
     </>
