7cd6ab82ab03c8035567d7b0036f0c6ffcd222f2
jessica <jessicajoseph2019@yahoo.com>
2023-04-18 01:31:14 +0100

    fix:btn link fix

--- pages/business.js
+++ pages/business.js
@@ -1,10 +1,59 @@
-import ClientForm from '@/src/form/ClientForm'
-import React from 'react'
+import ClientForm from "@/src/form/ClientForm";
+import Head from "next/head";
 
 const Business = () => {
   return (
-    <ClientForm/>
-  )
-}
+    <>
+      <Head>
+        <title>Innovotio</title>
+        <meta
+          name="description"
+          content="Innovotio is a tech business support organization that specializes in
+      connecting tech talent across all levels of experience to organizations
+      of all sizes to achieve their business goals."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.svg" />
+        <meta
+          property="og:url"
+          content="/https://innovotio-website.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Innovotio" />
+        <meta
+          property="og:description"
+          content="Innovotio is a tech business support organization that specializes in
+        connecting tech talent across all levels of experience to organizations
+        of all sizes to achieve their business goals."
+        />
+        <meta
+          property="og:image"
+          content="/https://innovotio-website.vercel.app/logo.svg"
+        />
 
-export default Business
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="innovotio.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="/https://innovotio-website.vercel.app/"
+        />
+        <meta name="twitter:title" content="Innovotio" />
+        <meta
+          name="twitter:description"
+          content="Innovotio is a tech business support organization that specializes in
+        connecting tech talent across all levels of experience to organizations
+        of all sizes to achieve their business goals."
+        />
+        <meta
+          name="twitter:image"
+          content="/https://innovotio-website.vercel.app/logo.svg"
+        />
+      </Head>
+      <section>
+        <ClientForm />
+      </section>
+    </>
+  );
+};
+
+export default Business;
