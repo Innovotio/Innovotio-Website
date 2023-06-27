7c0117958f07e42666fbb1b9361889714b86c44c
jessica <jessicajoseph2019@yahoo.com>
2023-06-27 20:56:47 +0100

    fix

--- src/components/AboutHero.js
+++ src/components/AboutHero.js
@@ -4,7 +4,7 @@ const AboutHero = () => {
   return (
     <section
       data-aos="zoom-in-right"
-      className="text-white md:px-10 px-5 xl:pt-64 2xl:pt-40 lg:pt-40 md:pt-16 pt-14 mx-auto container"
+      className="text-white md:px-10 px-5 xl:pt-48 lg:pt-28 md:pt-16 pt-14 mx-auto container"
     >
       <h1 className="md:text-5xl text-4xl font-medium mb-7">Who We Are</h1>
       <p className="text-justify lg:text-xl text-base text-headerGrey max-w-xl md:max-w-3xl font-light">
