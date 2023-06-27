3871b4f4e639a3d9edaadb38d44f3be1f611942a
jessica <jessicajoseph2019@yahoo.com>
2023-06-27 22:17:42 +0100

    corrections

--- src/components/Operations.js
+++ src/components/Operations.js
@@ -3,7 +3,7 @@ import YellowBtn from "./Micro/Button/YellowBtn";
 
 function Operations() {
   return (
-     <section className="mx-auto container flex-col lg:flex-row lg:items-center lg:justify-evenly border-t border-b  text-white lg:mt-32 md:mt-24 mt-14 py-12 border-btnDefaultBlack leading-tight flex ">
+     <section className="mx-auto container flex-col lg:flex-row lg:items-center lg:justify-evenly border-t border-b px-5 text-white lg:mt-32 md:mt-24 mt-14 py-12 border-btnDefaultBlack leading-tight flex ">
       <div data-aos="fade-right">
         <h1 className=" text-3xl font-medium max-w-lg lg:max-w-xl lg:text-5xl tracking-wide">
           Get swift and efficient career and business support{" "}
