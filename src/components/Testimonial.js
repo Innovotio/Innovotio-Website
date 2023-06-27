071e03ede779e9c048bd2f747d655ae96441f22b
jessica <jessicajoseph2019@yahoo.com>
2023-06-27 21:13:40 +0100

    fix

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -10,7 +10,7 @@ const Testimonial = () => {
     speed: 500,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 2800,
+    autoplaySpeed: 3000,
     infinite: true,
     pauseOnHover: true,
     arrows: false,
