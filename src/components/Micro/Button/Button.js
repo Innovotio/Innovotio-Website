78cd17ef4d3ca960aa3287da6efed376c9dfa77f
jessica <jessicajoseph2019@yahoo.com>
2023-07-05 17:32:26 +0100

    fix: header

--- src/components/Micro/Button/Button.js
+++ src/components/Micro/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({className, children}) => {
   return (
-    <button className={`${className} h-12 w-52  font-medium rounded-md`}>
+    <button className={`${className} h-12 w-52 hover:scale-105 font-medium rounded-md`}>
       {children}
     </button>
   );
